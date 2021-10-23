16fd109517b6eb8a43fdefcf91a439c888132270
yehor96 <yehor.shyriaiev@gmail.com>
2021-10-23 17:11:18 +0300

    complete AccountDbInitializer task

--- account-db-initializer/src/main/java/com/bobocode/AccountDbInitializer.java
+++ account-db-initializer/src/main/java/com/bobocode/AccountDbInitializer.java
@@ -1,6 +1,8 @@
 package com.bobocode;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
@@ -28,6 +30,22 @@ public class AccountDbInitializer {
      * @throws SQLException
      */
     public void init() throws SQLException {
-        throw new UnsupportedOperationException("It's your job to make it work!"); // todo
+        String sql = "CREATE TABLE account (" +
+                "id BIGINT NOT NULL" +
+                ",email VARCHAR(255) NOT NULL" +
+                ",first_name VARCHAR(255) NOT NULL" +
+                ",last_name VARCHAR(255) NOT NULL" +
+                ",gender VARCHAR(255) NOT NULL" +
+                ",birthday DATE NOT NULL" +
+                ",balance DECIMAL(19,4)" +
+                ",creation_time TIMESTAMP NOT NULL DEFAULT now()" +
+                ",CONSTRAINT account_pk PRIMARY KEY(id) " +
+                ",CONSTRAINT account_email_uq UNIQUE(email) " +
+                ");";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.execute();
+        }
     }
 }
